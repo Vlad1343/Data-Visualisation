464239fd94dae16c64ce944d5670309397ece142
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-16 13:41:24 +0300

    Matplotlib - Plotting squared numbers

---
+++ Matplotlib/Square/mpl_squares.py
@@ -0,0 +1,19 @@
+
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
+
+# Set name for graph and axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set font size for labels on the markup
+ax.tick_params(axis='both', labelsize=14)
+
+plt.show()
